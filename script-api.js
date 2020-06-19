2823ace13e3efe1e1ddd4a8f2072174c0684f6f7
dennisx <denizponce16@gmail.com>
2020-06-19 16:34:35 -0500

    Se ha implementado el select para el tipo de preguntas y los card para las preguntas

--- script-api.js
+++ script-api.js
@@ -1,18 +1,25 @@
-const endpoint = 'https://opentdb.com/api.php?amount=10&category=32&type=multiple'
-
-
 
 // Funcion para obtener las categorias
 function getCategories(){
     const endpointCategories = 'https://opentdb.com/api_category.php'
     fetch(endpointCategories)
-    .then((response) => response.json())
-    .then((dataJson) => {
-        printCategories(dataJson.trivia_categories)
-    })
-    .catch((error) => console.error(error))
+        .then((response) => response.json())
+        .then((dataJson) => {
+            printCategories(dataJson.trivia_categories)
+        })
+        .catch((error) => console.error(error))
 }
 
+
+function getQuestions(){
+    const endpointQuestions = 'https://opentdb.com/api.php?amount=10'
+    fetch(endpointQuestions)
+        .then((response) => response.json())
+        .then((myJson) => {
+            printQuestions(myJson.results)
+        })
+        .catch((error) => console.error(error))
+}
 // 1. obtener elemento donde se desea imprimir
 // 2. generar HTML
 // 3. imprimir informacion
@@ -31,5 +38,13 @@ function getCategoriesID(){
     console.log(`ha seleccionado la categoria que tiene el id ${categoryID}`)
 }
 
+function printQuestions(questions){
+    const typeQuestion = document.getElementById('select-typeQuestion')
+    let questionHTML = ''
+    
+    
+    
+}
 
+getQuestions()
 getCategories()
